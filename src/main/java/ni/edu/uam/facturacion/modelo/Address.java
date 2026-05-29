8638b2bba5dd4433aac323880756265c60b9a57d
Jesy05 <jgonzalezj@uamv.edu.ni>
2026-05-29 16:16:43 -0600

    feat: agregar clase Address

---
+++ src/main/java/ni/edu/uam/facturacion/modelo/Address.java
@@ -0,0 +1,30 @@
+package ni.edu.uam.facturacion.modelo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import java.io.StringReader;
+
+
+@Embeddable
+@Getter @Setter
+
+
+public class Address {
+
+    @Column(length = 30)
+    String street;
+
+    @Column(length = 5)
+    int zipCode;
+
+    @Column(length = 20)
+    String city;
+
+    @Column(length = 30)
+    String state;
+
+
+}
