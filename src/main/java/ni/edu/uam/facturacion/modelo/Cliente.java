6a2a5292787f3d3c603aa6912a15b2ba1a69a84f
Jesy05 <jgonzalezj@uamv.edu.ni>
2026-05-30 18:19:12 -0600

    feat: Cambio a costumer/cliente

--- src/main/java/ni/edu/uam/facturacion/modelo/Cliente.java
+++ src/main/java/ni/edu/uam/facturacion/modelo/Cliente.java
@@ -3,10 +3,9 @@ package ni.edu.uam.facturacion.modelo;
 import lombok.Getter;
 import lombok.Setter;
 import org.apache.logging.log4j.core.config.plugins.validation.constraints.Required;
+import org.openxava.annotations.NoFrame;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter @Setter
@@ -19,5 +18,9 @@ public class Cliente {
     @Column(length=50)
     @Required //es como un not null
     String nombre;
+
+    @Embedded @NoFrame
+    Address address;
+
 
 }
