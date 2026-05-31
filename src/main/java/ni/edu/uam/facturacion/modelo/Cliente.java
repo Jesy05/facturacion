7263d13342582865bb952159a56dcffe600ac351
Jesy05 <jgonzalezj@uamv.edu.ni>
2026-05-30 19:48:54 -0600

    feat: cambios a cliente

--- src/main/java/ni/edu/uam/facturacion/modelo/Cliente.java
+++ src/main/java/ni/edu/uam/facturacion/modelo/Cliente.java
@@ -2,25 +2,23 @@ package ni.edu.uam.facturacion.modelo;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.apache.logging.log4j.core.config.plugins.validation.constraints.Required;
-import org.openxava.annotations.NoFrame;
-
 import javax.persistence.*;
+import org.openxava.annotations.*;
 
 @Entity
 @Getter @Setter
-
+@View(name = "Simple", members = "numero, nombre")
 public class Cliente {
+
     @Id
-    @Column(length=6)
+    @Column(length = 6)
     int numero;
 
-    @Column(length=50)
-    @Required //es como un not null
+    @Column(length = 50)
+    @Required
     String nombre;
 
     @Embedded @NoFrame
     Address address;
-
 
 }
